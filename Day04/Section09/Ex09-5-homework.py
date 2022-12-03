49700facd978df0c6e11fb55131e3f978aed37af
ITSC <wpflxha6@gmail.com>
2022-12-03 09:51:35 +0900

    day05

--- Day04/Section09/Ex09-5-homework.py
+++ Day04/Section09/Ex09-5-homework.py
@@ -64,10 +64,99 @@ if cont >= 3:
             password = input('아이디가 맞지 않습니다 다시 입력하세요 >>>')
 
         print("환영합니다 {}님".format(Id))
+
     else:
         print('불가능한 비밀번호 입니다')
 
 else:
     print('id를 다시 입력하세요(3글자 이상)')
+
+
+
+
+
+
+# 선생님 버전
+
+Id = None
+pwd = None
+
+# 아이디 입력
+while True:
+    Id = input('아이디를 입력하세요(3글자 이상) >>>')
+
+    # id_couunt = 0
+    # for ch in id:
+    #     id_couunt += 1
+    #
+    # if id_couunt > 3:
+    #     break
+
+    if len(Id) > 2:
+        break
+    print("> 3글자 이상 입력해 주세요")
+
+# 패스워드 입력
+while True:
+    pwd = input('패쓰워드를 입력하세요(영문, 숫자 포함 8자 이상) >>>')
+    isContainAlpha = False
+    isContainNumeric = False
+    if len(pwd) < 8:
+        print('> 영문, 숫자 포함 8자 이상 입력해 주세요.')
+        continue
+    for ch in pwd:
+        if ch.isalpha():
+            isContainAlpha = True
+        elif ch.isnumeric():
+            isContainNumeric = True
+
+    # 영문 포함 유효성 검사
+    if not isContainAlpha:
+        print('> 영문, 숫자 포함 8자 이상 입력해 주세요.')
+        continue
+
+    # 숫자 포함 유효성 검사
+    if not isContainNumeric:
+        print('> 영문, 숫자 포함 8자 이상 입력해 주세요.')
+        continue
+
+    #패쓰워드 한번 더 확인 유효성 검사
+    pwdChk = input('패쓰워드를 한번 더 입력하세요 >>>')
+    if pwd != pwdChk:
+        print("> 일치하지 않습니다. 다시 입력해 주세요")
+        continue
+
+    break
+
+print('회원가입 완료!')
+
+
+# 로그인 아이디
+while True:
+    loginId = input('아이디를 입력하세요 >>>')
+    if Id == loginId:
+        break
+    print('> 아이디가 일치하지 않습니다.')
+
+# 로그인 패쓰워드
+while True:
+    loginPwd = input('패쓰워드를 입력하세요 >>')
+    if pwd == loginPwd:
+        break
+    print("패쓰워드가 일치 하지 않습니다.")
+
+print('로그인 성공!')
+print("환영합니다 {}님".format(Id))
+
+
+
+
+
+
+
+
+
+
+
 
 
